9ced0cd0bd62f08225760939882329f9f16da1ff
Soren Mathiasen <soren@schantz.com>
2013-06-26 09:39:30 +0200

    More documentation and comments

--- src/main/scala/com/scalaprog/todoapp/application/TodoApplicationService.scala
+++ src/main/scala/com/scalaprog/todoapp/application/TodoApplicationService.scala
@@ -6,22 +6,39 @@ import com.scalaprog.todoapp.domain.model.Todo
 import java.util.UUID
 import com.scalaprog.todoapp.domain.model.command.{CreateTask, CreateTodo}
 
+/**
+ * Application service for handling all things related to the Todo aggregate
+ * @param repository
+ */
 class TodoApplicationService(repository: TodoRepository) {
 
   type TodoId = UUID
 
+  /**
+   * Create a new Todo based on the command
+   * @param cmd
+   */
   def createTodo(cmd: CreateTodo) {
     val todo = new Todo(cmd.id)
     todo.createTodo(cmd.name, cmd.dueDate)
     repository.save(todo)
   }
 
+  /**
+   * Create a task on a given Todo
+   * @param cmd
+   */
   def createTask(cmd: CreateTask) {
     val todo: Todo = findTodo(cmd.todoId)
     todo.createTask(cmd.name, cmd.dueDate)
     repository.save(todo)
   }
 
+  /**
+   * Find a Todo in the repository using an Id
+   * @param id
+   * @return
+   */
   def findTodo(id: TodoId) = {
     repository.findTodoById(id)
   }
--- src/main/scala/com/scalaprog/todoapp/domain/model/TodoRepository.scala
+++ src/main/scala/com/scalaprog/todoapp/domain/model/TodoRepository.scala
@@ -2,6 +2,12 @@ package com.scalaprog.todoapp.domain.model
 
 import java.util.UUID
 
+/**
+ * Todo Repository for handling all persistence related operations on a Todo.
+ * The follows the Repository pattern see:
+ * http://blog.8thlight.com/mike-ebert/2013/03/23/the-repository-pattern.html or
+ * http://martinfowler.com/eaaCatalog/repository.html
+ */
 trait TodoRepository {
   type TodoId = UUID
 
